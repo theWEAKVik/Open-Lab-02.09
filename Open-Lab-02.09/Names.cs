44b6e5087a3a9938094dc57f51fa4e8882dd23b5
Viktor <krpectv57@gmail.com>
2020-10-06 15:23:50 +0200

    ""

--- Open-Lab-02.09/Names.cs
+++ Open-Lab-02.09/Names.cs
@@ -6,7 +6,8 @@ namespace Open_Lab_02._09
     {
         public string ConcatName(string firstName, string lastName)
         {
-            throw new NotImplementedException();
+            string contactname = (lastName + ", " + firstName);
+            return contactname;
         }
     }
 }
